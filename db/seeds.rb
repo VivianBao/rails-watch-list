ae40f8fe37f4800b012c50ee8c0a19f5e1ec57bb
Vivian Bow <vivianbao1009@gmail.com>
2021-11-11 18:15:01 +0900

    Routes, controllers basic style complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+puts 'Cleaning data...'
+Movie.destroy_all
+List.destroy_all
+
+puts 'Generating new data...'
+
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+movies_serialized = URI.open(url).read
+result = JSON.parse(movies_serialized)
+movies = result['results']
+# list of movie hashes
+
+genre_url = 'http://tmdb.lewagon.com/genre/movie/list'
+genres_serialized = URI.open(genre_url).read
+genre_result = JSON.parse(genres_serialized)
+genres_info = genre_result['genres']
+# list of genre hashes
+genre_ids = []
+
+movies.each do |movie|
+  Movie.create!(
+    title: movie['title'],
+    overview: movie['overview'],
+    poster_url: "https://image.tmdb.org/t/p/w500/#{movie['poster_path']}",
+    rating: movie['vote_average']
+  )
+  genre_id = movie['genre_ids'][0]
+  genre_ids << genre_id
+end
+
+genres_info.each do |genre|
+  genre_ids.uniq.each do |id|
+    if genre['id'] == id
+      List.create!(
+        name: genre['name']
+      )
+    end
+  end
+end
+
+puts "#{Movie.count} movies generated!"
+puts "#{List.count} lists generated!"
